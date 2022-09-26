9058d4b2e61cac0bb973e4ef28219da0138724f6
Jonathan Slack <slack6000@gmail.com>
2022-09-26 10:00:08 -0500

    second commit

--- 92622.js
+++ 92622.js
@@ -28,3 +28,19 @@ function getGrade (s1, s2, s3) {
     else if (avg < 90) return "B";
     else return "A";
 }
+
+//Best solution and then one I was trying to attempt
+function getGrade(s1, s2, s3) {
+  let meanScore = (s1 + s2 + s3) / 3;
+  if (meanScore >= 90 && meanScore <= 100) {
+    return "A";
+  } else if (meanScore >= 80 && meanScore < 90) {
+    return "B";
+  } else if (meanScore >= 70 && meanScore < 80) {
+    return "C";
+  } else if (meanScore >= 60 && meanScore < 70) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
